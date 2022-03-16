9d0d108bc658ded57e9b2a9e355a664157f5ba1a
Christian <a.christian.toscano@gmail.com>
2022-03-16 23:54:03 +0100

    using dotenv

--- app/services/geolocation_ip.rb
+++ app/services/geolocation_ip.rb
@@ -2,10 +2,11 @@ require 'net/http'
 
 class GeolocationIp
   class << self
-    API_KEY = Env.geoip_api_key
+    API_KEY = ENV['GEOIP_API_KEY']
 
     def call(ip:)
       return {} if ip == '::1'
+      return {} if API_KEY.nil?
 
       response = Net::HTTP.get(URI("https://api.freegeoip.app/json/#{ip}?apikey=#{API_KEY}"))
       response = JSON.parse(response, symbolize_names: true)

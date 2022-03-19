0c7ff401e5d4b6dc12db1376cc227da8826549e7
Christian <a.christian.toscano@gmail.com>
2022-03-19 16:48:20 +0100

    replacing email with username

--- db/migrate/20220106153955_devise_create_users.rb
+++ db/migrate/20220106153955_devise_create_users.rb
@@ -37,7 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :username,             unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true

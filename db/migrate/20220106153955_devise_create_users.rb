c2099ae4dbd83d648bec9aa61bd27286178b9e87
Christian <a.christian.toscano@gmail.com>
2022-03-26 15:47:38 +0100

    update user

--- db/migrate/20220106153955_devise_create_users.rb
+++ db/migrate/20220106153955_devise_create_users.rb
@@ -38,7 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     end
 
     add_index :users, :username,             unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_26_171115) do
     t.string "role", default: "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index "\"email\"", name: "index_users_on_email", unique: true
     t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "visits", force: :cascade do |t|

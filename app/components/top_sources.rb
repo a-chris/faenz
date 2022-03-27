7c04ea547fd8f462905390c008d7f7f46f1f4048
Christian <a.christian.toscano@gmail.com>
2022-03-27 01:06:51 +0100

    hide nil values from top_sources

--- app/components/top_sources.rb
+++ app/components/top_sources.rb
@@ -2,6 +2,6 @@ class TopSources < ViewComponent::Base
   def initialize(domain:, start_date:)
     @domain     = domain
     @start_date = start_date
-    @series     = @domain.visits.where('time_at > ?', start_date).pluck(:referrer).tally
+    @series     = @domain.visits.where.not(referrer: nil).where('time_at > ?', start_date).pluck(:referrer).tally
   end
 end

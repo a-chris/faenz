678253324557bdf57db0f8e5abf9ddea4c2eb167
Christian <a.christian.toscano@gmail.com>
2022-03-28 11:12:54 +0200

    fix bounce rate for real

--- app/components/top_stats.rb
+++ app/components/top_stats.rb
@@ -5,7 +5,7 @@ class TopStats < ViewComponent::Base
     @unique_visitors  = @domain.visits.pluck(:ip).uniq.size
     @pageviews        = @domain.visits.count
     single_page_session   = @domain.visits.pluck(:ip).tally.select { |k,v| v == 1 }.count
-    @bounce_rate          = single_page_session.positive? ? (single_page_session / @domain.visits.count).round : 0
+    @bounce_rate          = single_page_session.positive? ? ((single_page_session  * 100) / @domain.visits.count).round : 0
   end
 
   def single_page_session

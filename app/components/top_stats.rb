7324deed476d93e1cce27ee5ce7247051d81e147
Christian <a.christian.toscano@gmail.com>
2022-03-28 11:09:53 +0200

    fix bounce_rate calculation

--- app/components/top_stats.rb
+++ app/components/top_stats.rb
@@ -2,14 +2,10 @@ class TopStats < ViewComponent::Base
   def initialize(domain:, start_date:)
     @domain     = domain
     @start_date = start_date
-
-    ip_to_visits          = @domain.visits.pluck(:ip).tally
-    single_page_session   = ip_to_visits.select { |k,v| v == 1 }.count
-    multiple_page_session = ip_to_visits.select { |k,v| v > 1 }.count
-
     @unique_visitors  = @domain.visits.pluck(:ip).uniq.size
     @pageviews        = @domain.visits.count
-    @bounce_rate      = single_page_session.positive? ? (single_page_session / multiple_page_session).round : 0
+    single_page_session   = @domain.visits.pluck(:ip).tally.select { |k,v| v == 1 }.count
+    @bounce_rate          = single_page_session.positive? ? (single_page_session / @domain.visits.count).round : 0
   end
 
   def single_page_session

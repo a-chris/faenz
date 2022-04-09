379255b8ce4b9905d0fe10d67fba67068fa5d47d
christian-toscano <toscano@extendi.it>
2022-04-09 23:28:06 +0200

    improving seeds for more real data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,28 @@ user ||= User.create!(username: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASS
 domain = Domain.find_or_create_by!(user_id: user.id, base_url: 'example.com')
 
 if Visit.count.zero?
+  ips_pool = 500.times.map { Faker::Internet.ip_v4_address }
+  referrers_pool = ['github.com', 'google.com', 'twitter.com', 'youtube.com', 'bing.com', 'facebook.com',
+                    'instagram.com']
+  urls_pool = [
+    '',
+    '/homepage',
+    '/blog',
+    '/about',
+    '/contact',
+    '/privacy',
+    '/terms',
+    *10.times.map(&:to_s)
+  ].map { |s| "#{domain.base_url}#{s}" }
+
   1_000.times.map do
     Visit.create!(
       domain_id: domain.id,
       width: [378, 768, 1024].sample,
       event: 'pageview',
-      url: ['',
-            '/homepage',
-            '/blog',
-            '/about',
-            '/contact',
-            '/privacy',
-            '/terms',
-            *10.times.map(&:to_s)].map { |s| "#{domain.base_url}#{s}" }.sample,
-      referrer: ['github.com', 'google.com', 'twitter.com', 'youtube.com', 'bing.com', 'facebook.com', 'instagram.com'].sample,
-      ip: Faker::Internet.public_ip_v4_address,
+      url: urls_pool.sample,
+      referrer: referrers_pool.sample,
+      ip: ips_pool.sample, # so we can simulate bounce rate
       geo: {
         lat: Faker::Address.latitude,
         lng: Faker::Address.longitude,

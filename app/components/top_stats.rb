aa2f46722de8acf5b88e84220fc74ca8f9de103b
christian-toscano <toscano@extendi.it>
2022-04-09 23:27:42 +0200

    top stats use start_date and optimize calcultions

--- app/components/top_stats.rb
+++ app/components/top_stats.rb
@@ -2,10 +2,12 @@ class TopStats < ViewComponent::Base
   def initialize(domain:, start_date:)
     @domain     = domain
     @start_date = start_date
-    @unique_visitors  = @domain.visits.pluck(:ip).uniq.size
-    @pageviews        = @domain.visits.count
-    single_page_session   = @domain.visits.pluck(:ip).tally.select { |_k, v| v == 1 }.count
-    @bounce_rate          = single_page_session.positive? ? ((single_page_session * 100) / @domain.visits.count).round : 0
+
+    visits                = @domain.visits.where('time_at > ?', @start_date)
+    @unique_visitors      = visits.pluck(:ip).uniq.size
+    @pageviews            = visits.count
+    single_page_session   = visits.pluck(:ip).tally.select { |_k, v| v == 1 }.count
+    @bounce_rate          = single_page_session.positive? ? ((single_page_session * 100) / @pageviews).round : 0
   end
 
   def single_page_session

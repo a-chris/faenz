7e7be767595b76c7a6394e6029004526ff341898
christian-toscano <toscano@extendi.it>
2022-04-10 18:17:27 +0200

    index show current_user domains

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -4,7 +4,7 @@ class DomainsController < ApplicationController
 
   # GET /domains or /domains.json
   def index
-    @domains = Domain.all
+    @domains = current_user.domains
   end
 
   # GET /domains/1 or /domains/1.json

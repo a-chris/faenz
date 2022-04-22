8cd5a2d5bc4fd5852bcf86efd9c9f12c200f8bc2
christian-toscano <toscano@extendi.it>
2022-04-22 23:24:35 +0200

    trying to generate render secret on demand

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 bundle install
+EDITOR="notexist" bundle exec rails credentials:edit
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate

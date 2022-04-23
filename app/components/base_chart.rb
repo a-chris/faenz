a06e7984092563121162c0c70e6eca37b9995093
christian-toscano <toscano@extendi.it>
2022-04-24 00:19:31 +0200

    changes primary color

--- app/components/base_chart.rb
+++ app/components/base_chart.rb
@@ -2,7 +2,7 @@ class BaseChart < ViewComponent::Base
   attr_reader :series
 
   @@default_text_color = '#2f2f2f'
-  @@default_colors = ['#B33951', '#e6aa68']
+  @@default_colors = ['#ff5778', '#e6aa68']
 
   @@options = {
     library: {

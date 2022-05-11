17d06223488bb9df76859ab51e5b56349ac117d9
christian-toscano <toscano@extendi.it>
2022-05-11 23:31:49 +0200

    improving cors rules

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
     origins do |source|
       origin = source.gsub(%r{^https?://}, '').gsub('www.', '')
-      puts "origin2 #{origin}"
-      Domain.find_by(base_url: origin).present?
+      allow  = Domain.find_by(base_url: origin).present?
+      puts "blocked #{origin}" unless allow
+      allow
     end
     resource '*', headers: :any, methods: %i[get post patch put options]
   end

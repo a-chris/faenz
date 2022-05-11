bd83306d4f0ea2bb602190f5dacf31e92db6d9a3
christian-toscano <toscano@extendi.it>
2022-05-11 23:15:50 +0200

    testing cors to block unknown hosts

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :options]
+    origins do |source|
+      origin = source.gsub(%r{^https?://}, '').gsub('www.', '')
+      puts "origin2 #{origin}"
+      Domain.find_by(base_url: origin).present?
+    end
+    resource '*', headers: :any, methods: %i[get post patch put options]
   end
 end

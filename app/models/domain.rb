c68f583b3e5d22a671415bf11b1c10382beff81f
a-chris <a.christian.toscano@gmail.com>
2022-07-09 16:59:18 +0200

    revieweing required Domain fields

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -3,14 +3,18 @@ class Domain < ApplicationRecord
   has_many :visits
 
   validate :validate_base_url
-  validate :validate_icon
 
   before_save :clean_base_url
+  before_save :set_default_icon
 
   # callbacks
 
   def clean_base_url
     self.base_url = self.base_url&.gsub(%r{/$}, '')
+  end
+
+  def set_default_icon
+    self.icon ="#{self.base_url}/favicon.ico" if self.icon.blank?
   end
 
   # methods
@@ -28,12 +32,7 @@ class Domain < ApplicationRecord
   end
 
   def validate_base_url
-    return errors.add(:base_url, 'is invalid') unless base_url.present?
-    return errors.add(:base_url, 'should not start with http:// or https://') if base_url.match?(%r{^http(s?)://})
-  end
-
-  def validate_icon
-    return errors.add(:icon, 'is invalid') unless icon.present?
-    return errors.add(:icon, 'should start with http:// or https://') unless icon.match?(%r{^http(s?)://})
+    return errors.add(:base_url, 'is empty') unless base_url.present?
+    # return errors.add(:base_url, 'should not start with http:// or https://') if base_url.match?(%r{^http(s?)://})
   end
 end

0292330577097dc88d30f795015213a4760318ea
a-chris <32082962+a-chris@users.noreply.github.com>
2023-12-08 23:18:50 +0100

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :domains
-  root 'application#index'
-  post 'api/event', to: 'events#create'
-  options 'api/event', to: 'events#create'
+  root "application#index"
+  post "api/event", to: "events#create"
+  options "api/event", to: "events#create"
 end

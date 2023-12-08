cc87f54e21d81e7f347fc808145b41240fcbbc7f
a-chris <32082962+a-chris@users.noreply.github.com>
2023-12-08 23:18:30 +0100

    update cors using rails cache

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,16 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins do |source|
-      if source.include?('api/event')
-        origin = Domain.hotname(source)
-        allow  = Domain.find_by(base_url: origin).present?
-        puts "blocked #{origin}" unless allow
-        allow
-      else
-        true
+      next true if source.include?("localhost")
+
+      origin = Domain.hotname(source)
+      allow = Rails.cache.fetch(origin, expires_in: 1.year) do
+        Domain.find_by(base_url: origin).present?
       end
+      puts "blocked #{origin}" unless allow
+
+      allow
     end
-    resource '*', headers: :any, methods: %i[get post patch put options]
+    resource "/api/*", headers: :any, methods: %i[post options]
   end
 end
